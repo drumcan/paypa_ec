d903f3135e794a8fff00af3afb0a2fd3c0652da7
Dan O'Connor <daniel.oconnor@me.com>
2015-07-01 13:37:46 -0500

    added Do Express Checkout flow

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require 'sinatra'
 require 'net/http'
 require 'uri'
 
+@token
+@payer_id
+
 post "/ec" do 
   resp = Net::HTTP.post_form URI('https://api-3t.sandbox.paypal.com/nvp'), {
   	'METHOD' => 'SetExpressCheckout',
@@ -25,28 +28,43 @@ get "/ec" do
 end
 
 get "/success" do
-  token = params['token']
-  payer_id = params['PayerID']
-  p token
-  p payer_id
+  @token = params['token']
+  @payer_id = params['PayerID']
+  erb :success
 end
-  
+
+post "/confirm" do
+  resp = Net::HTTP.post_form URI('https://api-3t.sandbox.paypal.com/nvp'), {
+  	'METHOD' => 'doExpressCheckout',
+  	'VERSION' => '109.0',
+  	'USER' => 'daniel.oconnor_api1.paypal.com',
+  	'PWD' => 'DM6DWAL8P83XQE5M',
+  	'SIGNATURE' => 'AIHR.P3zrJxO3juFwINITaOoNj5eAquTzGgFuExWtKuvvm.lW2bWM.iW',
+  	'PAYMENTREQUEST_0_AMT' => '10.00',
+  	'PAYMENTREQUEST_0_CURRENCYCODE' => 'USD',
+  	'PAYMENTREQUEST_0_PAYMENTACTION' => 'SALE',
+  	'TOKEN' => @token,
+  	'PAYERID' => @payer_id
+  }	
+
+  response = parse(URI.decode(resp.body).to_str)
+  p response
 
 
 
 
 
 def parse(http_response)
-    pairs = http_response.split("&")
-    response = {}
+  pairs = http_response.split("&")
+  response = {}
 
-    pairs.each do |node|
-    parse_element(response, node)
-  end
-
-   response
+  pairs.each do |node|
+  parse_element(response, node)
 end
 
-  def parse_element(response, pair)
-        response[pair.split("=").first] = pair.split("=").last
-      end
+  response
+end
+
+def parse_element(response, pair)
+  response[pair.split("=").first] = pair.split("=").last
+end
